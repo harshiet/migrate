cfc2376fed8c918e54d5456c04237d6d4241a500
hagarwal <hagarwal@DX7QGT1.ceb.com>
2013-08-23 16:24:22 -0400

    1

--- migrate-core/src/main/java/com/kaanha/core/migrate/engine/RallyToJiraTransformationEngine.java
+++ migrate-core/src/main/java/com/kaanha/core/migrate/engine/RallyToJiraTransformationEngine.java
@@ -1,20 +1,14 @@
 package com.kaanha.core.migrate.engine;
 
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.logging.Logger;
 
-import org.apache.commons.lang.StringUtils;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.kaanha.migrate.core.persistence.domain.ArtifactType;
-import com.kaanha.migrate.core.persistence.domain.SystemAttribute;
 import com.kaanha.migrate.core.persistence.domain.SystemX;
 
 public class RallyToJiraTransformationEngine extends TransformationEngine {
@@ -55,31 +49,35 @@ public class RallyToJiraTransformationEngine extends TransformationEngine {
 	}
 
 	private JsonObject mapValues(JsonObject sourceObject, Map<String, String> map, ArtifactType artifactType) {
-//		Collection<String> attributes = target.getAttributesFor(artifactType).values();
-//		for (String attribute : attributes) {
-//			SystemAttribute mappedAttribute = attribute.getAttributeMapping(source);
-//			if (mappedAttribute != null) {
-//				String attributeMapping = mappedAttribute.getName();
-//				JsonElement attributeValue = sourceObject.get(attributeMapping);
-//
-//				while (mappedAttribute.hasChild()) {
-//					if (attributeValue == null) {
-//						logger.warning("Null " + attributeMapping);
-//						break;
-//					}
-//					if (attributeValue.isJsonNull()) {
-//						break;
-//					}
-//					mappedAttribute = mappedAttribute.getChildAttribute();
-//					attributeMapping = mappedAttribute.getName();
-//					attributeValue = attributeValue.getAsJsonObject().get(attributeMapping);
-//				}
-//				if (attributeValue != null && StringUtils.isNotBlank(attributeValue.getAsString())) {
-//					map.put(attribute.getName(), attributeValue.getAsString());
-//				}
-//			}
-//		}
-//
+		// Collection<String> attributes =
+		// target.getAttributesFor(artifactType).values();
+		// for (String attribute : attributes) {
+		// SystemAttribute mappedAttribute =
+		// attribute.getAttributeMapping(source);
+		// if (mappedAttribute != null) {
+		// String attributeMapping = mappedAttribute.getName();
+		// JsonElement attributeValue = sourceObject.get(attributeMapping);
+		//
+		// while (mappedAttribute.hasChild()) {
+		// if (attributeValue == null) {
+		// logger.warning("Null " + attributeMapping);
+		// break;
+		// }
+		// if (attributeValue.isJsonNull()) {
+		// break;
+		// }
+		// mappedAttribute = mappedAttribute.getChildAttribute();
+		// attributeMapping = mappedAttribute.getName();
+		// attributeValue =
+		// attributeValue.getAsJsonObject().get(attributeMapping);
+		// }
+		// if (attributeValue != null &&
+		// StringUtils.isNotBlank(attributeValue.getAsString())) {
+		// map.put(attribute.getName(), attributeValue.getAsString());
+		// }
+		// }
+		// }
+		//
 		Gson gson = new GsonBuilder().disableHtmlEscaping().create();
 		JsonObject json = (JsonObject) (new JsonParser()).parse(gson.toJson(map));
 		return json;
