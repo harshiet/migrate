e68e5a08ac0ca2912b54e4cd95e69d40fe450b3f
hagarwal <hagarwal@DX7QGT1.ceb.com>
2013-09-04 16:42:13 -0400

    1

--- migrate-web/src/main/java/com/kannha/migrate/web/controller/ConnectAction.java
+++ migrate-web/src/main/java/com/kannha/migrate/web/controller/ConnectAction.java
@@ -1,13 +1,19 @@
 package com.kannha.migrate.web.controller;
 
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.binding.message.MessageBuilder;
 import org.springframework.binding.message.MessageContext;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ConnectAction {
 
+	@Autowired
 	MigrationService migrationService;
-
+	Logger logger = Logger.getLogger(this.getClass().getName());
 	public boolean connect(MigrationRequest migrationRequest, MessageContext messageContext) {
+
 		boolean success = true;
 		try {
 			migrationService.connectSource(migrationRequest);
