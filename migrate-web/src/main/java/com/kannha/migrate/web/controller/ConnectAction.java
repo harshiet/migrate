30444ab7f85042b0d0f2d4dc584538464ca60560
Harsh Agarwal <harsh.agarwal82@gmail.com>
2013-09-06 12:41:57 -0400

    1

--- migrate-web/src/main/java/com/kannha/migrate/web/controller/ConnectAction.java
+++ migrate-web/src/main/java/com/kannha/migrate/web/controller/ConnectAction.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.binding.message.MessageBuilder;
 import org.springframework.binding.message.MessageContext;
 import org.springframework.stereotype.Service;
+import org.swift.common.soap.jira.RemoteAuthenticationException;
 
 @Service
 public class ConnectAction {
@@ -12,22 +13,33 @@ public class ConnectAction {
 	@Autowired
 	MigrationService migrationService;
 	Logger logger = Logger.getLogger(this.getClass().getName());
-	public boolean connect(MigrationRequest migrationRequest, MessageContext messageContext) {
+
+	public boolean connect(MigrationRequest migrationRequest,
+			MessageContext messageContext) {
 
 		boolean success = true;
 		try {
 			migrationService.connectSource(migrationRequest);
 		} catch (Exception e) {
-			logger.error(e.getMessage(),e);
+			logger.error(e.getMessage(), e);
 			success = false;
-			messageContext.addMessage(new MessageBuilder().error().defaultText(e.getMessage()).build());
+			messageContext.addMessage(new MessageBuilder().error()
+					.defaultText(e.getMessage()).build());
 		}
 		try {
 			migrationService.connectTarget(migrationRequest);
-		} catch (Exception e) {
-			logger.error(e.getMessage(),e);
+		} catch (RemoteAuthenticationException e) {
+			logger.error(e.getMessage(), e);
 			success = false;
-			messageContext.addMessage(new MessageBuilder().error().defaultText(e.getMessage()).build());
+			messageContext.addMessage(new MessageBuilder().error()
+					.defaultText("Invalid username and password.").build());
+		} catch (Exception e) {
+			if (e.getMessage() != null) {
+				logger.error(e.getMessage(), e);
+				success = false;
+				messageContext.addMessage(new MessageBuilder().error()
+						.defaultText(e.getMessage()).build());
+			}
 		}
 		return success;
 
